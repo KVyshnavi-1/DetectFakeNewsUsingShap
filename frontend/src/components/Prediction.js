f2fa52d66906175fa01b10b026cee67fa4d5eee7
KVyshnavi-1 <vyshukambhampati630@gmail.com>
2026-02-23 10:13:35 +0530

    Upated with actual backend deployed url

--- frontend/src/components/Prediction.js
+++ frontend/src/components/Prediction.js
@@ -24,7 +24,7 @@ function Prediction({ addToHistory }) {
 
     try {
 
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://detectfakenewsusingshap.onrender.com/predict", {
 
         method: "POST",
 
